6904d451241ed76cbb8e9f71aa2463bb1ce8cf05
alex <alexchen@champimom.com>
2025-05-14 11:18:52 +0800

    first commit

---
+++ .prettierrc.cjs
@@ -0,0 +1,7 @@
+module.exports = {
+  plugins: ['prettier-plugin-tailwindcss'],
+  printWidth: 100, // 每行最多 100 字
+  semi: false, // 不加分號
+  singleQuote: true, // 使用單引號
+  trailingComma: 'none' // 不加尾逗號
+}
---
+++ nuxt.config.ts
@@ -0,0 +1,12 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: '2024-11-01',
+  devtools: { enabled: true },
+  devServer: {
+    port: 8080
+  },
+  modules: ['@nuxt/ui', '@nuxtjs/supabase'],
+  supabase: {
+    redirect: false
+  }
+})
